4a2fda62178025a35f0d92fb88da249b872c3945
sharlettcho-being <sharlettcho@beingcorp.co.jp>
2024-04-16 17:37:37 +0900

    #485954 [C#テキスト]3章演習問題
    3章の演習問題を解きました。

---
+++ 章未問題3/Practice3-1-1/Program3-1-1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> {12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48};
+
+            Console.WriteLine("リストでの要素を８か９で割り切れる数があるかどうか？");
+            //質問①＝＞リストでの要素を８か９で割り切れる数があるかどうか
+            var exist = numbers.Exists(x => x % 8 == 0 || x % 9 == 0);
+            Console.WriteLine(exist);
+
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("リストの各要素を2.0で割った値");
+
+            //質問②＝＞リストの各要素を2.0で割った値
+            numbers.ForEach(x => Console.WriteLine(x / 2.0));
+
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("リストでの値が50以上の要素");
+
+            //質問③＝＞リストでの値が50以上の要素
+            var number = numbers.Where(x => x >= 50);
+            foreach(var item in number)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("リストでの値を2倍にした結果");
+
+            //質問④＝＞リストでの値を2倍にしてリストに格納する
+            List<int> query = numbers.Select(x => x * 2).ToList();
+            foreach(var item in query)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
---
+++ 章未問題3/Practice3-1-2/Program3-1-2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3_1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = -1;
+            var names = new List<string> {"Tokyo", "New Delhi", "Bangkok", "London", "Paris",
+                        "Berlin", "Canberra", "Hong Kong"};
+
+            //質問①=>FindIndexを使って入力された名を調べる
+            Console.WriteLine("都市名を記入お願いします！");
+            string wCityName = Console.ReadLine();
+
+            Console.WriteLine("入力された名が何番目にあるか？");
+
+            index = names.FindIndex(s => s.ToLower() == wCityName.ToLower());
+            if (index > 0)
+                Console.WriteLine(index);
+            else Console.WriteLine(index);
+
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("小文字 'o'が含まれている都市名のカウント");
+
+            //質問②=>小文字 'o'が含まれている都市名のカウントを計算する
+            int count = names.Where(s => s.Contains('o')).Count();
+            Console.WriteLine(count);
+
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("小文字 'o'が含まれている都市名");
+
+            //質問③=>Whereを使って、小文字 'o'が含まれている都市名を表示する
+            var count2 = names.Where(s => s.Contains('o')).ToArray();
+            foreach(var cityname in count2)
+            {
+                Console.WriteLine(cityname);
+            }
+
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("'B'で始まる都市名の文字数");
+
+            //質問④=>'B'で始まる都市名の文字数
+            var result = names.Where(x => x[0] == 'B').Select(x => x.Length);
+            foreach(var length in result)
+            {
+                Console.WriteLine(length);
+            }            
+
+        }
+    }
+}
