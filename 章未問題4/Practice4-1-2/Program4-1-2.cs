48b29a9c3707462b43759cae477ea4fc7c5b1260
sharlettcho-being <sharlettcho@beingcorp.co.jp>
2024-04-24 11:07:44 +0900

    #485955 [C#テキスト]4章演習問題
    レビュー指摘を修正しました。

--- 章未問題4/Practice4-1-2/Program4-1-2.cs
+++ 章未問題4/Practice4-1-2/Program4-1-2.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Practice4_1_1;
 
-namespace Practice4_1_2
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            YearMonth[] yearMonths = new YearMonth[5]
-            {
+namespace Practice4_1_2 {
+    internal class Program{
+        static void Main(string[] args){
+            YearMonth[] wYearMonths = new YearMonth[5]{
             new YearMonth(2124, 4),
             new YearMonth(2124, 5),
             new YearMonth(2124, 6),
@@ -22,39 +15,31 @@ namespace Practice4_1_2
 
             Console.WriteLine("--------配列にある初期値---------");
             // 配列の要素を表示する
-            foreach (YearMonth yearMonth in yearMonths)
-            {
+            foreach (YearMonth yearMonth in wYearMonths) {
                 Console.WriteLine(yearMonth);
             }
 
             //最初の21世紀を調べる
-            YearMonth result = FindFirst21stCenturyYearMonth(yearMonths);
-            if(result != null)
-            {
-                Console.WriteLine($"最初にみつかった21世紀：　{result}");
+            var wFirst21Century = FindFirst21stCenturyYearMonth(wYearMonths);
+            if(wFirst21Century != null){
+                Console.WriteLine($"最初にみつかった21世紀：　{wFirst21Century}");
             }
-            else
-            {
+            else{
                 Console.WriteLine("21世紀のデータはありません");
             }
 
-            YearMonth[] nextMonths = yearMonths.Select(x => x.AddOneMonth()).ToArray();
+            YearMonth[] wOneMonthAfterChangedMonth = wYearMonths.Select(x => x.AddOneMonth()).ToArray();
 
             Console.WriteLine("--------1カ月後に変更した値---------");
 
-            foreach (YearMonth nextMonth in nextMonths)
-            {
+            foreach (YearMonth nextMonth in wOneMonthAfterChangedMonth) {
                 Console.WriteLine(nextMonth);
             }
-
         }
         //最初にみつかった21世紀を調べる
-        public static YearMonth FindFirst21stCenturyYearMonth(YearMonth[] yearMonths)
-        {
-            foreach (YearMonth yearMonth in yearMonths)
-            {
-                if (yearMonth.Is21Century)
-                {
+        public static YearMonth FindFirst21stCenturyYearMonth(YearMonth[] vYearMonths){
+            foreach (YearMonth yearMonth in vYearMonths) {
+                if (yearMonth.Is21Century){
                     //見つかったYearMonthを返す
                     return yearMonth;
                 }
--- 章未問題4/Practice4-1-2/YearMonth.cs
+++ 章未問題4/Practice4-1-2/YearMonth.cs
@@ -1,52 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Practice4_1_1
-{
-    internal class YearMonth
-    {
+﻿namespace Practice4_1_1 {
+    /// <summary>
+    /// Year,Monthを持っているクラス
+    /// </summary>
+    internal class YearMonth{
+        /// <summary>
+        /// 年の値を取得します。
+        /// </summary>
+        /// /// <remarks>
+        /// このプロパティは年の値を表します。
+        /// </remarks>
         public int Year { get; private set;}
+
+        /// <summary>
+        /// 月の値を取得します。
+        /// </summary>
+        /// <remarks>
+        /// このプロパティは月の値を表します。
+        /// </remarks>
         public int Month { get; private set; }
 
-        public YearMonth(int year, int month)
-        {
-            this.Year = year;
-            this.Month = month;
+        public YearMonth(int vYear, int vMonth){
+            this.Year = vYear;
+            this.Month = vMonth;
         }
-        public bool Is21Century
-        {
-            get
-            {
+
+        /// <summary>
+        /// 21世紀のデータかどうかのチェック
+        /// </summary>
+        public bool Is21Century{
+            get{
                 return Year >= 2001 && Year <= 2100;
             }
         }
 
-        public YearMonth AddOneMonth()
-        {
-            YearMonth newYearMonth = new YearMonth(Year, Month);
-            newYearMonth.Year = Year;
-            newYearMonth.Month = Month;
+        /// <summary>
+        /// 1カ月後を求める
+        /// </summary>
+        /// <returns>1カ月後の年・月を返す</returns>
+        public YearMonth AddOneMonth(){
+            YearMonth wNewYearMonth = new YearMonth(Year, Month);
+            wNewYearMonth.Year = Year;
+            wNewYearMonth.Month = Month;
 
-            if (Month == 12)
-            {
-                newYearMonth.Year++;
-                newYearMonth.Month = 1;
+            if (Month == 12){
+                wNewYearMonth.Year++;
+                wNewYearMonth.Month = 1;
             }
-            else
-            {
-                newYearMonth.Month++;
+            else{
+                wNewYearMonth.Month++;
             }
-
-            return newYearMonth;
-            
+            return wNewYearMonth;            
         }
-        public override string ToString()
-        {
+
+        /// <summary>
+        /// ToStringメソッドをオーバーライドする
+        /// </summary>
+        /// <returns>〇〇年○○月の形を返す</returns>
+        public override string ToString(){
             return ($"{Year}年{Month}月");
         }
-
     }
 }
