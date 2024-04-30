4d2ab3ae7cce27dab6bc8ff87331b10141d3b7b5
sharlettcho-being <sharlettcho@beingcorp.co.jp>
2024-04-30 16:46:16 +0900

    #485964 [C#テキスト]11章演習問題
    11章の演習問題を解きました。

---
+++ 章未問題11/Practice11-1-1/Program11-1-1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Practice11_1_1 {
+    internal class Program {
+        /// <summary>
+        /// XMLファイルを読み込んで処理する
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args) {
+            string wXMLFilePath = "C:\\Users\\sharlettcho\\Desktop\\Sample11-1.xml";
+            var wXDoc = XDocument.Load(wXMLFilePath);
+            var wElements = wXDoc.Root.Elements();
+
+            DisplayGamenameAndTeammembreCount(wElements);
+            DisplaySportsByFirstPlayed(wElements);
+            DisplaySportWithMostMember(wElements);
+            AddSoccerInfoAndSaveToFile(wXDoc, @"C:\Users\sharlettcho\Desktop\NewSports.xml", "サッカー", 11, 1863);
+
+        }
+
+        /// <summary>
+        /// XMLファイルから教示名とチームメンバー数を表示する
+        /// </summary>
+        /// <param name="vElements"></param>
+        public static void DisplayGamenameAndTeammembreCount(IEnumerable<XElement> vElements) {
+            foreach (var wElement in vElements) {
+                XElement wName = wElement.Element("name");
+                var wTeamMember = wElement.Element("teammembers");
+                Console.WriteLine("{0} : {1}", wName.Value, wTeamMember.Value);
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// 最初にプレーされた年の若い順に漢字の競技名を表示する
+        /// </summary>
+        /// <param name="vElements"></param>
+        public static void DisplaySportsByFirstPlayed(IEnumerable<XElement> vElements) {
+            var wPlayerList = vElements.OrderBy(x => (int.Parse(x.Element("firstplayed").Value)));
+            foreach (var wPlayer in wPlayerList) {
+                Console.WriteLine(wPlayer.Element("name").Attribute("kanji").Value);
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// メンバー人数が最も多い競技名を表示する
+        /// </summary>
+        /// <param name="vElements"></param>
+        public static void DisplaySportWithMostMember(IEnumerable<XElement> vElements) {
+            var wMaxCount = vElements.Max(x => (int.Parse(x.Element("teammembers").Value)));
+            var wSportWithMostMembers = vElements.FirstOrDefault(x => int.Parse(x.Element("teammembers").Value) == wMaxCount);
+
+            Console.WriteLine(wSportWithMostMembers.Element("name").Value);
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// サッカーの情報を追加し、新たなXMLファイルを出力する
+        /// </summary>
+        /// <param name="vXdoc">存在しているドキュメント</param>
+        /// <param name="vFilePath">新たな作成するファイルのパス</param>
+        /// <param name="vSoccerName">サッカー名</param>
+        /// <param name="vSoccerTeamMembers">チームメンバー数</param>
+        /// <param name="vSoccerFirstPlayed">プレーされた年</param>
+        public static void AddSoccerInfoAndSaveToFile(XDocument vXdoc, string vFilePath, string vSoccerName, int vSoccerTeamMembers, int vSoccerFirstPlayed) {
+            XElement soccerElement = new XElement("ballsport",
+            new XElement("name", new XAttribute("kanji", "蹴球"), vSoccerName),
+            new XElement("teammembers", vSoccerTeamMembers),
+            new XElement("firstplayed", vSoccerFirstPlayed));
+            vXdoc.Root.Add(soccerElement);
+
+            // 新しいXMLファイルに保存
+            vXdoc.Save(vFilePath);
+
+            Console.WriteLine("サッカーの情報を追加した新しいXMLファイルが作成されました！ "); 
+        }
+    }
+}
---
+++ 章未問題11/Practice11-1-2/Program11-1-2.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+
+namespace Practice11_1_2 {
+    internal class Program {
+        /// <summary>
+        /// XMLファイルの形式を変換し、別のファイルに保存する
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args) {
+            string wXMLFilePath = "C:\\Users\\sharlettcho\\Desktop\\Sample11-2.xml";
+            var wXDoc = XDocument.Load(wXMLFilePath);
+   
+            // 新しいXMLを作成
+            XDocument wNewDoc = new XDocument(
+                new XDeclaration("1.0", "utf-8", ""),
+                new XElement("difficultkanji",
+                    // 各word要素を変換
+                    wXDoc.Root.Elements("word").Select(x =>
+                        new XElement("word",
+                        new XAttribute("kanji", x.Element("kanji").Value),
+                        new XAttribute("yomi", x.Element("yomi").Value)
+                        )
+                    )
+                )
+            );
+            wNewDoc.Save("C:\\Users\\sharlettcho\\Desktop\\Sample11 -2New.xml");
+        }
+    }
+}
