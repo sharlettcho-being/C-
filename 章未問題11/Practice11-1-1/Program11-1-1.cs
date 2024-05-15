c44896cc0b290ac338d21efd30b0ef6ae98dc1a3
sharlettcho-being <sharlettcho@beingcorp.co.jp>
2024-05-15 13:54:03 +0900

    #485964 [C#テキスト]11章演習問題
    レビューの指摘を修正しました。

--- 章未問題11/Practice11-1-1/Program11-1-1.cs
+++ 章未問題11/Practice11-1-1/Program11-1-1.cs
@@ -1,22 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 
 namespace Practice11_1_1 {
     internal class Program {
         static void Main(string[] args) {
-            // 実行されるアプリケーションのカレントディレクトリを取得
-            string wCurrentDirectory = Directory.GetCurrentDirectory();
-
             // 相対パスを使用してファイルにアクセスする例
-            string wRelativePath = @"..\..\Sample11-1.xml";
-            string wSaveFileRelativePath = @"..\..\Sample11-1.xml";
-
-            // 実際のファイルパスを取得
-            string wXMLFilePath = Path.Combine(wCurrentDirectory, wRelativePath);
-            string wSaveFilePath = Path.Combine(wCurrentDirectory, wSaveFileRelativePath);
+            string wXMLFilePath = @"..\..\Sample11-1.xml";
+            string wSaveFilePath = @"..\..\Sample11-1.xml";
 
             var wXDoc = XDocument.Load(wXMLFilePath);
             var wElements = wXDoc.Root.Elements();
@@ -25,7 +17,6 @@ namespace Practice11_1_1 {
             DisplaySportsByFirstPlayed(wElements);
             DisplaySportWithMostMember(wElements);
             AddSoccerInfoAndSaveToFile(wXDoc, wSaveFilePath, "サッカー", 11, 1863);
-
         }
 
         /// <summary>
@@ -34,7 +25,7 @@ namespace Practice11_1_1 {
         /// <param name="vElements">XMLファイルの要素</param>
         public static void DisplayGamenameAndTeammembreCount(IEnumerable<XElement> vElements) {
             foreach (var wElement in vElements) {
-                XElement wName = wElement.Element("name");
+                var wName = wElement.Element("name");
                 var wTeamMember = wElement.Element("teammembers");
                 Console.WriteLine($"{wName.Value} : {wTeamMember.Value}");
             }
@@ -46,7 +37,7 @@ namespace Practice11_1_1 {
         /// </summary>
         /// <param name="vElements">XMLファイルの要素</param>
         public static void DisplaySportsByFirstPlayed(IEnumerable<XElement> vElements) {
-            var wPlayerList = vElements.OrderBy(x => (int.Parse(x.Element("firstplayed").Value)));
+            var wPlayerList = vElements.OrderBy(x => int.Parse(x.Element("firstplayed").Value));
             foreach (var wPlayer in wPlayerList) {
                 Console.WriteLine(wPlayer.Element("name").Attribute("kanji").Value);
             }
@@ -56,7 +47,7 @@ namespace Practice11_1_1 {
         /// <summary>
         /// メンバー人数が最も多い競技名を表示する
         /// </summary>
-        /// <param name="vElements"></param>
+        /// <param name="vElements">XMLファイルの要素</param>
         public static void DisplaySportWithMostMember(IEnumerable<XElement> vElements) {
             var wMaxCount = vElements.Max(x => int.Parse(x.Element("teammembers").Value));
             var wSportWithMostMembers = vElements.FirstOrDefault(x => int.Parse(x.Element("teammembers").Value) == wMaxCount);
@@ -74,7 +65,7 @@ namespace Practice11_1_1 {
         /// <param name="vSoccerTeamMembers">チームメンバー数</param>
         /// <param name="vSoccerFirstPlayed">プレーされた年</param>
         public static void AddSoccerInfoAndSaveToFile(XDocument vXdoc, string vFilePath, string vSoccerName, int vSoccerTeamMembers, int vSoccerFirstPlayed) {
-            XElement soccerElement = new XElement("ballsport",
+            var soccerElement = new XElement("ballsport",
             new XElement("name", new XAttribute("kanji", "蹴球"), vSoccerName),
             new XElement("teammembers", vSoccerTeamMembers),
             new XElement("firstplayed", vSoccerFirstPlayed));
--- 章未問題11/Practice11-1-2/Program11-1-2.cs
+++ 章未問題11/Practice11-1-2/Program11-1-2.cs
@@ -1,31 +1,23 @@
-﻿using System.IO;
-using System.Linq;
+﻿using System.Linq;
 using System.Xml.Linq;
 
 namespace Practice11_1_2 {
     internal class Program {
         static void Main(string[] args) {
-            // 実行されるアプリケーションのカレントディレクトリを取得
-            string wCurrentDirectory = Directory.GetCurrentDirectory();
-
             // 相対パスを使用してファイルにアクセスする例
-            string wRelativePath = @"..\..\Sample11-2.xml";
+            string wXMLFilePath = @"..\..\Sample11-2.xml";
 
-            string wSaveFileRelativePath = @"..\..\Sample11 -2New.xml";
-
-            // 実際のファイルパスを取得
-            string wXMLFilePath = Path.Combine(wCurrentDirectory, wRelativePath);
-
-            string wSaveFilePath = Path.Combine(wCurrentDirectory, wSaveFileRelativePath);
+            string wSaveFilePath = @"..\..\Sample11 -2New.xml";
 
             var wXDoc = XDocument.Load(wXMLFilePath);
    
             // 新しいXMLを作成
             XDocument wNewDoc = new XDocument(
                 new XDeclaration("1.0", "utf-8", ""),
-                    new XElement("difficultkanji",
-                    // 各word要素を変換
-                    wXDoc.Root.Elements("word").Select(x =>
+                new XElement("difficultkanji",
+                // 各word要素を変換
+                wXDoc.Root.Elements("word")
+                    .Select(x =>
                         new XElement("word",
                         new XAttribute("kanji", x.Element("kanji").Value),
                         new XAttribute("yomi", x.Element("yomi").Value)
