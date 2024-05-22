232fc58edddea8b872a15d5dbdde316bc247cf18
sharlettcho-being <sharlettcho@beingcorp.co.jp>
2024-05-22 17:26:32 +0900

    #493162 [C#テキスト]12章演習問題のテスト用
    12章のロパティを読み取り専用にし、逆シリアル化に試した処理を解きました。

---
+++ 章未問題12_テスト用/Practice12-1-1/Program12-1-1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Text;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.Xml;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Xml.Schema;
+
+namespace Practice12_1_1 {
+    /// <summary>
+    /// 従業員クラス
+    /// </summary>
+    [XmlRoot("employee")]
+    [DataContract(Name = "employee")]
+    public class Employee {
+        /// <summary>
+        /// 従業員の一意の識別子
+        /// </summary>
+        [XmlElement(ElementName = "id")]
+        [IgnoreDataMember]
+        [DataMember(Name = "id")]
+        public int Id { get; private set; }
+
+        /// <summary>
+        /// 従業員の名前
+        /// </summary>
+        [XmlElement(ElementName = "name")]
+        [DataMember(Name = "name")]
+        public string Name { get;  private set; }
+
+        /// <summary>
+        /// 従業員の採用日
+        /// </summary>
+        [XmlElement(ElementName = "hiredate")]
+        [DataMember(Name = "hiredate")]
+        public DateTime HireDate { get; private set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Employee() { }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="vId">従業員の一意の識別子</param>
+        /// <param name="vName">従業員の名前</param>
+        /// <param name="vHireDate">従業員の採用日</param>
+        public Employee(int vId, string vName, DateTime vHireDate) {
+            this.Id = vId;
+            this.Name = vName;
+            this.HireDate = vHireDate;
+        }
+
+        /// <summary>
+        /// オブジェクトをシリアル化にする。
+        /// </summary>
+        public void SerializeObject(Employee vEmployees, XmlWriterSettings vSettings, string vFileName) {
+            using (var writer = XmlWriter.Create(vFileName, vSettings)) {
+                var wSerializer = new XmlSerializer(typeof(Employee));
+                wSerializer.Serialize(writer, vEmployees);
+            }
+        }
+
+        /// <summary>
+        /// 複数のオブジェクトが格納されている配列をシリアル化にする。
+        /// </summary>
+        /// <param name="vEmployees">配列</param>
+        public  void SerializeObjectArray(Employee[] vEmployees, XmlWriterSettings vSettings, string vFileName) {
+            using (var wWriter = XmlWriter.Create(vFileName, vSettings)) {
+                var wSerializer = new DataContractSerializer(vEmployees.GetType());
+                wSerializer.WriteObject(wWriter, vEmployees);
+            }
+        }
+
+        /// <summary>
+        /// 複数のオブジェクトが格納されている配列を逆シリアル化
+        /// </summary>
+        public  void DeserializeObjectArray(string vFileName) {
+            using (var wReader = XmlReader.Create(vFileName)) {
+                var wDeserializer = new DataContractSerializer(typeof(Employee[]));
+                var wEmployees = wDeserializer.ReadObject(wReader) as Employee[];
+                foreach (var wEmployee in wEmployees) {
+                    Console.WriteLine($"Id: {wEmployee.Id}\nName: {wEmployee.Name}\nHireDate: {wEmployee.HireDate}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 複数のオブジェクトが格納されている配列をJSONファイルに出力する。
+        /// Idはシリアル化対象外。
+        /// </summary>
+        /// <param name="vEmployees">配列</param>
+        public  void SerializeObjectArrayToJsonFile(Employee[] vEmployees, DataContractJsonSerializerSettings vJsonSettings) {
+            using (var wStream = new FileStream("Employee.json", FileMode.Create, FileAccess.Write)) {
+                var wSerializer = new DataContractJsonSerializer(typeof(Employee[]), vJsonSettings);
+                wSerializer.WriteObject(wStream, vEmployees);
+            }
+        }
+    }
+
+    internal class Program {
+        static void Main(string[] args) {
+            var wEmployees = new Employee[] {
+                new Employee(1, "桜子", DateTime.Today),
+                new Employee (2, "桜華", new DateTime(2024, 6, 29)),
+                new Employee (3, "桜太郎", new DateTime (2024,04,25))
+            };
+
+            // XMLシリアライゼーション設定
+            var wSettings = new XmlWriterSettings {
+                Encoding = new UTF8Encoding(false),
+                Indent = true,
+                IndentChars = " ",
+            };
+
+            // JSONシリアライゼーション設定
+            var wJsonSettings = new DataContractJsonSerializerSettings {
+                //日付の表示についての設定
+                DateTimeFormat = new DateTimeFormat("yyyy-MM-dd'T'HH:mm:ss")
+            };
+
+            var wFileName = "Employee.xml";
+
+            var wNewEmployee = new Employee();
+
+            //オブジェクトをシリアル化にする。
+            wNewEmployee.SerializeObject(wEmployees[0], wSettings, wFileName);
+
+            //複数のオブジェクトが格納されている配列をシリアル化にする。
+            wNewEmployee.SerializeObjectArray(wEmployees, wSettings, wFileName);
+
+            //複数のオブジェクトが格納されている配列を逆シリアル化にする。
+            wNewEmployee.DeserializeObjectArray(wFileName);
+
+            //複数のオブジェクトが格納されている配列をJSONファイルに出力する
+            wNewEmployee.SerializeObjectArrayToJsonFile(wEmployees, wJsonSettings);
+        }
+
+        
+    }
+}
---
+++ 章未問題12_テスト用/Practice12-1-2/Program12-1-2.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Windows.Markup;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Practice12_1_2 {
+    /// <summary>
+    /// 小説家の情報を表すクラス
+    /// </summary>
+    [XmlRoot("novelist")]
+    [DataContract(Name = "novel")]
+    public class NoveList {
+        /// <summary>
+        /// 小説家の名前を取得または設定
+        /// </summary>
+        [XmlElement(ElementName = "name")]
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 小説家の生年月日を取得または設定
+        /// </summary>
+        [XmlElement(ElementName = "birth")]
+        [DataMember(Name = "birth")]
+        public DateTime Birth { get; set; }
+
+        /// <summary>
+        /// 小説家の代表作のリストを取得または設定
+        /// </summary>
+        [XmlArray("masterpeices")]
+        [DataMember(Name = "masterpiece")]
+        [XmlArrayItem("title", typeof(string))]
+        public string[] Masterpieces { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public NoveList() { }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="name">小説家の名前</param>
+        /// <param name="birth">小説家の生年月日</param>
+        /// <param name="masterpieces">小説家の代表作のリスト</param>
+        public NoveList(string vName, DateTime vBirth, string[] vMasterpieces) {
+            this.Name = vName;
+            this.Birth = vBirth;
+            this.Masterpieces = vMasterpieces;
+        }
+    }
+
+    internal class Program {
+        static void Main(string[] args) {
+            var wNovelist = new NoveList(
+                "アーサー・C・クラーク",
+                new DateTime(1917, 12, 16),
+                new string[] {
+                    "2001年守宙の旅",
+                    "幼年期の終り"
+                }
+            );
+
+            //Xmlファイルでの設定
+            var wSettings = new XmlWriterSettings {
+                Indent = true,
+                IndentChars = " ",
+                Encoding = Encoding.UTF8,
+            };
+
+            // 名前空間の出力を制御するためのXmlSerializerNamespacesオブジェクトを作成
+            var wNamespaces = new XmlSerializerNamespaces();
+            wNamespaces.Add("", ""); // 名前空間を空に設定
+
+            //XmlSerializerを使ってシリアル化にする
+            using (var wWriter = XmlWriter.Create("novelist.xml", wSettings)) {
+                var wXmlSerializer = new XmlSerializer(wNovelist.GetType());
+                wXmlSerializer.Serialize(wWriter, wNovelist, wNamespaces);
+            }
+
+            //XmlSerializerを使って逆シリアル化にする
+            using (var wReader = XmlReader.Create("novelist.xml")) {
+                var wDeserializer = new XmlSerializer(typeof(NoveList));
+                var wNoveList = wDeserializer.Deserialize(wReader) as NoveList;
+                Console.WriteLine($"{wNoveList.Name}\n{wNoveList.Birth}");
+                foreach (var wNovel in wNoveList.Masterpieces) {
+                    Console.WriteLine(wNovel);
+                }
+            }
+
+            //日付の表示についての設定
+            var wJsonSettings = new DataContractJsonSerializerSettings {
+                DateTimeFormat = new DateTimeFormat("yyyy-MM-dd'T'HH:mm:ss"),
+            };
+
+            //JSONファイルに逆シリアル化にする
+            using (var wStream = new FileStream("novel.json", FileMode.Create, FileAccess.Write))
+            using (var wJsonWriter = JsonReaderWriterFactory.CreateJsonWriter(wStream, Encoding.UTF8, true, true, " ")) {
+                var wJsonSerializer = new DataContractJsonSerializer(typeof(NoveList), wJsonSettings);
+                wJsonSerializer.WriteObject(wJsonWriter, wNovelist);
+            }
+        }
+    }
+}
